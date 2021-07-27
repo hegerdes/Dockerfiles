16e3b28a6879c2bbf5dbd7efdfc7de4b733dcce5
Henrik Gerdes <hegerdes@outlook.de>
2021-07-28 00:16:19 +0200

    Added arg for pandoc in script

--- LaTeX/build.sh
+++ LaTeX/build.sh
@@ -3,13 +3,27 @@
 IMAGE_NAME="hegerdes/vscode-latex"
 VARIANTS=(debian:buster-slim debian:bullseye-slim ubuntu:focal ubuntu:bionic)
 LANGUAGES=(all arabic chinese cjk cyrillic czechslovak english european french german greek italian japanese korean other polish portuguese spanish)
+PANDOC_DOWNLOAD_URL="NONE"
+PANDOC_DOWNLOAD_URL_ARM="https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-1-arm64.deb"
+PANDOC_DOWNLOAD_URL_AMD="https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-1-amd64.deb"
 PUSH="FALSE"
+ARM="FALSE"
 
 #Set CWD
 cd "$(dirname "$0")"
-if [ $# -eq 1 ] && [ $1 = "push" ]; then
-    echo "Will build and push to dockerhub"
+PANDOC_DOWNLOAD_URL=$PANDOC_DOWNLOAD_URL_AMD
+if [ $1 = "push" ]; then
+    echo "Will build amd64 and push to dockerhub"
     PUSH="TRUE"
+fi
+
+if [ $1 = "arm" ]; then
+    PANDOC_DOWNLOAD_URL=$PANDOC_DOWNLOAD_URL_ARM
+    ARM="TRUE"
+    if [ $2 = "push" ]; then
+    echo "Will build arm and push to dockerhub"
+    PUSH="TRUE"
+    fi
 fi
 
 # Build base
@@ -17,7 +31,8 @@ for VARIANT in ${VARIANTS[@]}; do
     VARIANT_BUILD_TAG=(${VARIANT//:/ })
     VARIANT_NAME_TAG=(${VARIANT_BUILD_TAG[1]//-/ })
     echo "Building ${IMAGE_NAME}:${VARIANT_NAME_TAG[0]}..."
-    docker build --build-arg VARIANT=$VARIANT -f "Dockerfile.base" -t $IMAGE_NAME:${VARIANT_NAME_TAG[0]} .
+    echo "docker build --build-arg VARIANT=${VARIANT} --build-arg PANDOC_URL=${PANDOC_DOWNLOAD_URL} -f "Dockerfile.base" -t $IMAGE_NAME:${VARIANT_NAME_TAG[0]} ."
+    docker build --build-arg VARIANT=$VARIANT --build-arg PANDOC_URL=$PANDOC_DOWNLOAD_URL -f "Dockerfile.base" -t $IMAGE_NAME:${VARIANT_NAME_TAG[0]} .
     if [ $PUSH = "TRUE" ]; then
         docker push $IMAGE_NAME:${VARIANT_NAME_TAG[0]}
     fi
